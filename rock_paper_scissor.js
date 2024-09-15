52c908160021faedf5f16a5769bf79fbaf92fb4f
Thato <developer101@Xbuuntu.myguest.virtualbox.org>
2024-09-15 16:18:44 +0200

    Created functions getComputerChoice,getHumanChoice

---
+++ rock_paper_scissor.js
@@ -0,0 +1,30 @@
+//Creating random number generator
+function genRandom()
+{
+    return Math.floor(Math.random() * 3);
+}
+//Function for computer
+function getComputerChoice()
+{
+    ranNum=genRandom();
+    if(ranNum===0)
+    {
+        return "rock";
+    }
+    else if(ranNum===1)
+    {
+        return "paper";
+    }
+    else
+    {
+        return "scissors";
+    }
+}
+
+function getHumanChoice()
+{
+    //Getting userr input
+    let choice = prompt("Rock, Paper or Scissors?Pick one.");
+    return choice.toLowerCase();
+
+}
