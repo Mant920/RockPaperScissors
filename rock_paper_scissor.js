8ceaf00e745d9bd3f6bf9da753b056be2dd16206
Thato <developer101@Xbuuntu.myguest.virtualbox.org>
2024-09-15 17:14:22 +0200

    Created function playround

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -26,8 +26,60 @@ function getComputerChoice()
 
 function getHumanChoice()
 {
-    //Getting userr input
+    //Getting user input
     let choice = prompt("Rock, Paper or Scissors?Pick one.");
     return choice.toLowerCase();
 
 }
+
+function playRound(humanChoice, computerChoice)
+{
+    
+    let message=" ";
+
+    if((humanChoice==="rock" && computerChoice==="paper") || (humanChoice==="scissors" && computerChoice==="rock") || (humanChoice==="paper" && computerChoice==="scissors"))
+    {
+        
+
+        //Incrementing computer score
+        computerScore+=1;
+        if(humanChoice==="rock")
+        {
+            message="You lose, Paper beats Rock!";
+        }
+        else if(humanChoice==="scissors")
+        {
+            message="You lose, Rock beats Scissors!";
+        }
+        else if(humanChoice==="paper")
+        {
+            message="You lose, Scissors beats Paper !";
+        }
+        
+    }
+    else if((humanChoice==="paper" && computerChoice==="rock") || (humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="scissors" && computerChoice==="paper"))
+    {
+        //Incrementing human score
+        humanScore+=1;
+        if(humanChoice==="rock")
+        {
+            message="You win, Rock beats Scissors!";
+        }
+        else if(humanChoice==="scissors")
+        {
+            message="You win, Scissors beats Paper !";
+        }
+        else if(humanChoice==="paper")
+        {
+            message="You lose, Paper beats Rock !";
+        }
+        
+    }
+    else
+    {
+        message="It's a draw!";
+    }
+    return message;
+
+}
+
